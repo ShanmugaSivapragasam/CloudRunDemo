7d4b2751a78083840bf4340d8add8ef5c9cf5e5f
M_783062 <M_783062@macltus66992.attlocal.net>
2019-07-21 11:41:25 -0400

    [0] init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CloudRunDemo
 CloudRunDemo
+
+Quick `Cloud Run` sample with `Cloud Scheduler` and `Cloud Storage `
---
+++ src/main/java/com/shan/cloudrundemo/CloudrundemoApplication.java
@@ -0,0 +1,13 @@
+package com.shan.cloudrundemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudrundemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudrundemoApplication.class, args);
+	}
+
+}
